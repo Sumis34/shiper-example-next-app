1f62f6eed16f3c227b87dc668951865848097a3d
Noé <eon.sberk@gmail.com>
2023-07-07 11:07:35 +0200

    8

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  //7
+  //8
   console.log(request.headers.get("x-forwarded-host"));
 
   return NextResponse.json({ data: process.env });
